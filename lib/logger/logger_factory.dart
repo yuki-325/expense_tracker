5312b7c9a55f5415603fda67595be3dcf618164b
中野勇貴 <sakeikura325325@gmail.com>
2024-03-10 23:04:37 +0900

    [fix] logger メソッドとタイムスタンプを出力

--- lib/logger/logger_factory.dart
+++ lib/logger/logger_factory.dart
@@ -5,7 +5,13 @@ class LoggerFactory {
   static Logger getLogger() {
     return Logger(
       printer: CustomLoggerPrinter(
-        PrettyPrinter(colors: false, methodCount: 0),
+        PrettyPrinter(
+          colors: false,
+          methodCount: 1,
+          excludePaths: [
+            'package:expense_tracker/logger/custom_logger_printer.dart'
+          ],
+        ),
       ),
     );
   }
