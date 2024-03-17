794ef4f1a9c502a577a15e5308b87aa783df06c5
中野勇貴 <sakeikura325325@gmail.com>
2024-03-17 17:11:04 +0900

    [add] アプリケーションのベースのカラーを設定

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,27 @@ import 'package:expense_tracker/expenses.dart';
 import 'package:expense_tracker/logger/logger_factory.dart';
 import 'package:flutter/material.dart';
 
+ColorScheme kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(
+    225,
+    96,
+    59,
+    181,
+  ),
+);
+
 void main() {
   final logger = LoggerFactory.getLogger();
   logger.i("App Start!!");
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
       home: const Expenses(),
     ),
   );
